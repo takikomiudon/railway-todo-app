79540bdd8889ed24f1f4436d8d3b4894a2289cc1
takikomiudon <takikomiudon1743@gmail.com>
2024-03-02 22:43:20 +0900

    prettier

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
